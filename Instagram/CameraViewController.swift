0aa694f91d0c83244cbd70cea23e43920e8a32a3
Krina Patel <KRINA.PATEL96@myhunter.cuny.edu>
2022-10-08 15:12:03 -0400

    initial commit

---
+++ Instagram/CameraViewController.swift
@@ -0,0 +1,37 @@
+//
+//  CameraViewController.swift
+//  Instagram
+//
+//  Created by Krina Patel on 10/8/22.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var commentField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func onSubmitButton(_ sender: Any) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
